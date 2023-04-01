e9e3a7f8837950ffe8915fe72a0acf5019222857
Brandon-Schefstad <bschefstad@gmail.com>
2023-04-01 07:17:32 -0400

    Experimenting with vitest

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,11 +1,35 @@
-import { createClassDescription } from "functions-to-test";
+import { ClassDescription, Spell } from "@prisma/client";
+import { createClassDescription, createSpell } from "functions-to-test";
 import { expect, test, vi } from "vitest"; // 👈🏻 Added the `vi` import
 import prisma from "../libs/__mocks__/prisma";
 vi.mock("../libs/prisma");
 
+test("createClassDescription should return the generated class", async () => {
+  const newClass = { name: "Artificer" };
+  prisma.classDescription.create.mockResolvedValue({ ...newClass, id: 1 });
+  const createdClass: ClassDescription = await createClassDescription(newClass);
+  expect(createdClass).toStrictEqual({ ...newClass, id: 1 });
+});
 test("createUser should return the generated user", async () => {
-  const newUser = { name: "Artificer" };
-  prisma.classDescription.create.mockResolvedValue({ ...newUser, id: 1 });
-  const user = await createClassDescription(newUser);
-  expect(user).toStrictEqual({ ...newUser, id: 1 });
+  const newSpell = {
+    name: "Ability Rip",
+    level: 1,
+    school: "test",
+    range: 5,
+    duration: 1,
+    components: "S",
+    description: "X",
+  };
+  prisma.spell.create.mockResolvedValue({
+    ...newSpell,
+    id: 1,
+    concentration: false,
+    ritual: false,
+  });
+  const user: Spell = await createSpell(newSpell);
+  console.log(user);
+  expect(user).toStrictEqual({
+    ...newSpell,
+    id: 1,
+  });
 });
