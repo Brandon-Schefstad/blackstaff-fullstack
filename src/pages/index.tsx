71c0f2f93194fde81f0a20896b159cd26b7a3725
Brandon-Schefstad <bschefstad@gmail.com>
2023-04-02 21:51:23 -0400

    improve index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,35 @@
-import { ClassDescription } from "@prisma/client";
 import { type NextPage } from "next";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
-  const [data, setData] = useState([]);
-  const [isLoading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    fetch("/api/read/class")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-      });
-  }, []);
+  const names = [
+    "Artificer",
+    "Bard",
+    "Cleric",
+    "Druid",
+    "Paladin",
+    "Ranger",
+    "Sorcerer",
+    "Warlock",
+    "Wizard",
+  ];
 
   return (
     <section className="pt-8 pl-8 font-[amagro]">
-      {isLoading ? (
-        <></>
-      ) : (
-        <>
-          <h1 className="text-2xl">Blackstaff's Spell Emporium</h1>
-          <section className="grid grid-cols-4 gap-8 p-8 ">
-            {data ? (
-              data.map((classDescription: ClassDescription) => (
-                <Link href={classDescription.name}>
-                  <section
-                    className="grid h-[250px] justify-center rounded-lg border-2 border-solid border-black align-middle"
-                    key={classDescription.id}
-                  >
-                    {classDescription.name}
-                  </section>
-                </Link>
-              ))
-            ) : (
-              <></>
-            )}
-          </section>
-        </>
-      )}
+      <h1 className="text-2xl">Blackstaff's Spell Emporium</h1>
+      <section className="grid grid-cols-4 gap-8 p-8 ">
+        {names.map((nameOfClass: string, i: number) => (
+          <Link href={nameOfClass}>
+            <section
+              className="grid h-[250px] justify-center rounded-lg border-2 border-solid border-black align-middle"
+              key={i}
+            >
+              {nameOfClass}
+            </section>
+          </Link>
+        ))}
+      </section>
+
       <Link href="/getSpells">Click me </Link>
     </section>
   );
