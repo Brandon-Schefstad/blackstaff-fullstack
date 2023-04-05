eea57846b88a2f52df620ba37336cdc452184bc5
Brandon-Schefstad <bschefstad@gmail.com>
2023-04-05 16:49:42 -0400

    Making footer sticky

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className=" absolute bottom-8 min-w-full bg-black py-4 text-center text-white">
+    <div className=" sticky bottom-8 min-w-full bg-black py-4 text-center text-white">
       <a href="/">
         <h1 className="text-xl">Blackstaff's Spell Emporium</h1>
       </a>
