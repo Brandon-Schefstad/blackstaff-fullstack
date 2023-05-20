71ef43fc12fdb8b84873c9fe96158861ffa69aaa
Brandon Schefstad <bschefstad@gmail.com>
2023-05-19 20:11:44 -0400

    Update classes.tsx

--- src/pages/classes.tsx
+++ src/pages/classes.tsx
@@ -1,4 +1,5 @@
 import { type NextPage } from "next";
+import ClassListItem from "../components/ClassListItem";
 import Footer from "../components/Footer";
 import NavBar from "../components/Navbar";
 
@@ -16,14 +17,18 @@ const Classes: NextPage = () => {
   ];
 
   return (
-    <>
+    <div className="font-[amagro] text-black">
       <NavBar />
-      <div className=" grid  pt-8 ">
+      <div className="mt-4 grid   ">
         <h1 className="text-center text-2xl  ">Select Your Class</h1>
+        <ul className="grid gap-12  px-4 py-8 text-sm sm:grid-cols-3 sm:justify-between sm:gap-4 sm:text-lg lg:px-28">
+          {names.map((name: string, i) => {
+            return <ClassListItem name={name} i={i} />;
+          })}
+        </ul>
       </div>
       <Footer />
-    
-    </>
+    </div>
   );
 };
 
