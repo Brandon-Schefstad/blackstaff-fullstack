6aef5fea5d4cbdffc4ec2e403767dea0f83377cf
Brandon-Schefstad <bschefstad@gmail.com>
2023-06-16 14:11:39 -0400

    test suite

--- test/sample.test.js
+++ test/sample.test.js
@@ -1,12 +1,14 @@
-import { assert, test } from "vitest";
+import { assert, describe, test } from "vitest";
 import { spells } from "../data/parsing/parsedAndClean";
 
-test("should first", () => {
-  const spellsWithBadEndings = spells.filter(
-    (spell) => spell.effect.at(-1) !== "."
-  );
-  spellsWithBadEndings.forEach((spell) => {
-    console.log(spell.name, [spell.effect.at(-1)]);
+describe("DESCRIBE BLOCK", () => {
+  test("TEST BLOCK", () => {
+    const spellsWithBadEndings = spells.filter(
+      (spell) => spell.effect.at(-1) !== "."
+    );
+    spellsWithBadEndings.forEach((spell) => {
+      console.log(spell.name, [spell.effect.at(-1)]);
+    });
+    assert.equal(spellsWithBadEndings.length, 1);
   });
-  assert.equal(spellsWithBadEndings.length, 0);
 });
