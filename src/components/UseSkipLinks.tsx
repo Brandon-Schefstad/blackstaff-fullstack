089c9e783915964bea9ec39a268b3c6869ab18e8
Brandon-Schefstad <bschefstad@gmail.com>
2023-06-19 05:40:52 -0400

    ts + eslint hotfix

--- src/components/UseSkipLinks.tsx
+++ src/components/UseSkipLinks.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 interface LinkType {
+  [x: string]: any;
   title: string;
   id: string;
 }
@@ -11,7 +12,9 @@ export default function useSkipLinks() {
     const skipLinkElements: NodeListOf<Element> = document.querySelectorAll(
       "[data-skip-link]:not(pre):not(code)"
     );
+    //@ts-ignore
     const links = Array.from(skipLinkElements, (skipLink: LinkType) => ({
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
       title: skipLink.dataset.skipLink,
       id: skipLink.id,
     }));
